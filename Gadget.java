fb72e313545bd7b2f711948853b02383e6e380e4
fa-iqq29 <faiqmakkah29@gmail.com>
2025-08-20 20:47:07 +0700

    Add files via upload

---
+++ Gadget.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugascontoh2;
+
+/**
+ *
+ * @author Faiq
+ */
+public class Gadget {
+
+    /**
+     * @return the jenisDaya
+     */
+    protected String getJenisDaya() {
+        return jenisDaya;
+    }
+
+    /**
+     * @param jenisDaya the jenisDaya to set
+     */
+    protected void setJenisDaya(String jenisDaya) {
+        this.jenisDaya = jenisDaya;
+    }
+
+    /**
+     * @return the Berat
+     */
+    protected int getBerat() {
+        return Berat;
+    }
+
+    /**
+     * @param Berat the Berat to set
+     */
+    protected void setBerat(int Berat) {
+        this.Berat = Berat;
+    }
+    private String jenisDaya;
+    private int Berat; 
+}
